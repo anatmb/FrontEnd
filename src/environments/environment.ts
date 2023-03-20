0331aef4297837e01cf799d26b82746be118cd13
anatmb <anatmb26@hotmail.com>
2023-03-20 11:52:07 -0300

    colocando el url de render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,16 +14,8 @@ export const environment = {
     measurementId: 'G-D24G5NM4SK',
   },
   production: false,
-  URL:'http://localhost:8080',
+ // URL:'http://localhost:8080',
   //URL: para colocar las prodcuccion
-   //URL:'https://bankendfinal.onrender.com',
+   URL:'https://bankendfinal.onrender.com',
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
